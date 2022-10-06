f2db3b17568f627b92982679b8fef41bf901620e
Limdaram <clara9182@naver.com>
2022-10-06 19:51:15 +0900

    1006 오후 최종

---
+++ src/main/java/servlet/chap17/Servlet05.java
@@ -0,0 +1,50 @@
+package servlet.chap17;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+import jsp20220923.chap07.Book;
+
+/**
+ * Servlet implementation class Servlet05
+ */
+@WebServlet("/Servlet05")
+public class Servlet05 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet05() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* Book book = new Book("java", 500); */
+		List list = List.of(new Book("java", 500), new Book("css", 200), new Book("jsp", 300));
+		request.setAttribute("bookList", list);
+		String jspPath="/WEB-INF/view/chap17/view04.jsp";
+		request.getRequestDispatcher(jspPath).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
